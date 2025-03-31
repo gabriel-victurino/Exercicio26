9bd46797e23e9a0a0b7e986edbe8f54b0418b4f9
gabriel-victurino <gabriel.gbzzx@gmail.com>
2025-03-31 10:29:48 -0300

    Update Exercicio26.java

--- Exercicio26.java
+++ Exercicio26.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.exercicio26;
-
-/**
- *
- * @author 10725113959
- */
 import java.util.Scanner;
 public class Exercicio26 {
 
